924afb9268ea0d75a22d55f52243b1d5575c4848
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2021-11-29 17:22:24 -0800

    fixed some errors, moved wording around for easier understanding, added outline of Readme file to Generate Markdown file

--- index.js
+++ index.js
@@ -3,69 +3,75 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+// Function call to initialize app
+init();
 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-    type: "input",
-    name: "name",
-    message: "What is your name?"
-},
-{
-    type: "input",
-    name: "username",
-    message: "What is your Github username?"
-},
-{
-    type: "input",
-    name: "projectName",
-    message: "What is the name of your Project?"
-},
-{
-    type: "list",
-    name: "licenseType",
-    message: "Which license will be used for this project?"
-    choices: ["MIT", "Apache 2.0", "GPLv3", "BSD 3-Clause", "Creative Commons (CC0)", "LGPLv3", "AGPLv3", "Hippocratic 3.0", "WTFPL" ]
-},
-{
-    type: "input",
-    name: "description",
-    message: "Please enter a brief description of your project: "
-},
-{
-    type: "input",
-    name: "installation",
-    message: "What are the installation steps?"
-},
-{
-    type: "input",
-    name: "usage",
-    message: "Why would you use this project?"
-},
-{
-    type: "input",
-    name: "contribution",
-    message: "Who contributed to this project, besides yourself?"
+function init() {
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your Github username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your e-mail?"
+        },
+        {
+            type: "input",
+            name: "projectName",
+            message: "What is the name of your Project?"
+        },
+        {
+            type: "list",
+            name: "licenseType",
+            message: "Which license will be used for this project?",
+            choices: ["MIT", "Apache 2.0", "GPLv3", "BSD 3-Clause", "Creative Commons (CC0)", "LGPLv3", "AGPLv3", "Hippocratic 3.0", "WTFPL", "None" ]
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please enter a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the installation steps?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Why would you use this project?"
+        },
+        {
+            type: "input",
+            name: "contributions",
+            message: "Who contributed to this project, besides yourself?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What testing was done on this project?"
+        }
+    ])
+    .then((responses) => {
+        console.log(responses);
+        fs.writeToFile(README1.md, generateMarkdown(responses));
+    });
 }
-{
-    type: "input",
-    name: "tests",
-    message: "What testing was done on this project?"
-}
-];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((responses) =>{
-        console.log(responses);
-
-    })
-}
-
-// Function call to initialize app
-init();
+    fs.writeFile(fileName, data, (error) => {
+        if(error) throw error;
+        console.log("File is being made!");
+    });
+};
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+
+  // Badge will appear near top. info added to correct areas, add notice to license area, link to GH profile, email to questions area with instructions on how to reach me. TOC should have links to sections
+  return `# ${data.title}
+  
+
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributions](#contributions)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  To install this program and run it as intended, please use the following commands within your node.js server:
+
+  ${data.installation}
+
+  ## Usage
+
+    ${data.usage}
+
+  ## License
+
+  ${data.license}
+
+  ## Contributions
+  Here is the list of individuals who have contributed to this project!
+  
+  ${data.contributions}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+  If you have any questions, please feel free to contact me!
+   - [Github](https://github.com/${data.username})
+   - [Email](${data.email})
 `;
 }
 
